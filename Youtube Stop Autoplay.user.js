f1e28eb8bcc00c1ed16f711126fd12bb0855f2de
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-10-21 16:28:21 +0200

    Update Youtube Stop Autoplay.user.js

--- Youtube Stop Autoplay.user.js
+++ Youtube Stop Autoplay.user.js
@@ -1,13 +1,15 @@
 // ==UserScript==
 // @name         Youtube Stop Autoplay
 // @namespace    https://github.com/johnnyawesome
-// @version      1.0
+// @version      1.0.1
 // @description  Take over the world! :)
 // @author       JohnnyAwesome
 // @match        http://*
 // @match        https://*
 // @grant        none
 // @icon         https://cdn0.iconfinder.com/data/icons/pixelo/32/skull.png
+// @updateURL    https://github.com/johnnyawesome/UserscriptYoutubeStopAutoplay/raw/master/Youtube%20Stop%20Autoplay.user.js
+// @downloadURL  https://github.com/johnnyawesome/UserscriptYoutubeStopAutoplay/raw/master/Youtube%20Stop%20Autoplay.user.js
 // ==/UserScript==
 
 (function() {
