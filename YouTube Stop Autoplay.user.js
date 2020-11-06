dbb405771626a1923ec8896816cf323766d4e3bd
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2020-11-07 00:27:18 +0100

    Add files via upload

--- YouTube Stop Autoplay.user.js
+++ YouTube Stop Autoplay.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Stop Autoplay
 // @namespace    https://github.com/johnnyawesome
-// @version      1.0.11
+// @version      1.0.10
 // @description  Stops YouTube from auto-playing videos
 // @author       JohnnyAwesome
 // @match        http://www.youtube.com/*
@@ -20,11 +20,12 @@ setInterval(turnAutoplayOff, 10000);
 
 function turnAutoplayOff(){
 
-  let button = document.getElementsByClassName("ytp-autonav-toggle-button")[0];
+  //console.log("Autoplay is turned on:", document.getElementById("toggle").getAttribute("aria-pressed"));
 
-  if(button
-  && button.getAttribute("aria-checked") === "true"){
+  if(document.getElementById("toggle")
+  && document.getElementById("toggle").getAttribute("aria-pressed")
+  && document.getElementById("toggle").getAttribute("aria-pressed") == 'true'){
     console.log('%c YouTube Auto-Play is turned on. Turning it off. ', 'background: #000; color: #00ff00');
-    button.click();
+    document.getElementById("toggle").click();
   }
 }
